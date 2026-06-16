75512a8ab3a18976d3ff063d896dd1a1e98e6bc4
Aurefeune <47830708+Aurefeune@users.noreply.github.com>
2026-06-16 21:43:29 +0200

    Fix: Change block ID from 'royal_block' to 'kingdom_block' for consistency

--- src/main/java/net/avantguarde/kingdomlandmod/block/ModBlocks.java
+++ src/main/java/net/avantguarde/kingdomlandmod/block/ModBlocks.java
@@ -12,7 +12,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(Registries.BLOCK, KingdomLandMod.MOD_ID);
 
-    public static final RegistryObject<Block> KINGDOM_BLOCK = BLOCKS.register("royal_block",
+    public static final RegistryObject<Block> KINGDOM_BLOCK = BLOCKS.register("kingdom_block",
             () -> new KingdomBlock(BlockBehaviour.Properties.of()
                     .mapColor(MapColor.GOLD)
                     .strength(3.0f, 6.0f)));
